cb5cb7402f7eadb1ed06e93614d77987cdd0e43b
attilakoksal <attila.b.koksal@gmail.com>
2025-04-10 00:37:43 -0700

    inventory class almost complete

---
+++ Assets/Scripts/Interactables/Inventory.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Inventory : Interactable, IPointerClickHandler
+{
+    Image itemIcon;
+    public Item userItem { get; set; }
+    public InventorySlot slot { get; set; }
+
+    void Awake()
+    {
+        itemIcon = GetComponent<Image>();
+    }
+    public void Initialize(Item item, InventorySlot invenSlot)
+    {
+        slot = invenSlot;
+        slot.userItem = this;
+        userItem = item;
+        itemIcon.sprite = item.sprite;
+    }
+    public void onPointerClick(PointerEventData eventData)
+    {
+        if(eventData.button == PointerEventData.InputButton.Left)
+        {
+            Inventory.Singleton.SetCarriedItem(this);
+        }
+    }
+}
