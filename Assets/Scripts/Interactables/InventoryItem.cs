3fb3694e400b32db7d68d4cf0caa2dfa5bca45be
attilakoksal <attila.b.koksal@gmail.com>
2025-04-16 15:44:04 -0700

    KeyboardControlsDone

--- Assets/Scripts/Interactables/InventoryItem.cs
+++ Assets/Scripts/Interactables/InventoryItem.cs
@@ -21,43 +21,24 @@ public class InventoryItem : MonoBehaviour, IPointerClickHandler
         myItem = item;
         itemIcon.sprite = item.sprite;
     }
+
     public void OnPointerClick(PointerEventData eventData)
     {
         if(eventData.button == PointerEventData.InputButton.Left)
         {
             Inventory.Singleton.SetCarriedItem(this);
         }
-        else if(eventData.button == PointerEventData.KeyCode.1)
+    }
+
+    void KeyboardControls()
+    {
+        for(int i = 0; i < 8; i++)
         {
-            Inventory.Singleton.SetCarriedItem(this);
-        }
-        else if(eventData.button == PointerEventData.KeyCode.2)
-        {
-            Inventory.Singleton.SetCarriedItem(this);
-        }
-        else if(eventData.button == PointerEventData.KeyCode.3)
-        {
-            Inventory.Singleton.SetCarriedItem(this);
-        }
-        else if(eventData.button == PointerEventData.KeyCode.4)
-        {
-            Inventory.Singleton.SetCarriedItem(this);
-        }
-        else if(eventData.button == PointerEventData.KeyCode.5)
-        {
-            Inventory.Singleton.SetCarriedItem(this);
-        }
-        else if(eventData.button == PointerEventData.KeyCode.6)
-        {
-            Inventory.Singleton.SetCarriedItem(this);
-        }
-        else if(eventData.button == PointerEventData.KeyCode.7)
-        {
-            Inventory.Singleton.SetCarriedItem(this);
-        }
-        else if(eventData.button == PointerEventData.KeyCode.8)
-        {
-            Inventory.Singleton.SetCarriedItem(this);
+            if(Input.GetKeyDown(KeyCode.Alpha1+i))
+            {
+                Inventory.Singleton.SetCarriedItem(this);
+                break;
+            }
         }
     }
 }
