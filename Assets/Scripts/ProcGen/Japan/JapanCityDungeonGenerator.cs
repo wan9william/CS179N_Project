cda41dbb3eec5bf1016662078178c2b17b1b5a63
William W <wan9william@gmail.com>
2025-06-03 01:11:17 -0700

    layering

--- Assets/Scripts/ProcGen/Japan/JapanCityDungeonGenerator.cs
+++ Assets/Scripts/ProcGen/Japan/JapanCityDungeonGenerator.cs
@@ -343,7 +343,7 @@ public class JapanCityDungeonGenerator : JapanDungeonGenerator
             GameObject door = Instantiate(doorHorizontalPrefab, this.transform);
             doorList.Add(door);
             Vector2Int startPosition = (startPositionY + Direction2D.cardinalDirectionsList[1]);
-            Vector3 doorPosition = new Vector3(startPosition.x + 0.5f, startPosition.y, 0);
+            Vector3 doorPosition = new Vector3(startPosition.x + 0.5f, startPosition.y+0.5f, 0);
             door.transform.localPosition = doorPosition;
             doorPositions.Add(startPositionY + Direction2D.cardinalDirectionsList[1] * 2);
             doorPositions.Add(startPositionY + Direction2D.cardinalDirectionsList[1]);
