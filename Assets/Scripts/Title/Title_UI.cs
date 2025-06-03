ea28e8f864216abdcc2a11e7ad5ec44041896577
Nicholas Karalis <114453356+Googoo03@users.noreply.github.com>
2025-06-03 00:29:43 -0700

    Start Screen Functioning

---
+++ Assets/Scripts/Title/Title_UI.cs
@@ -0,0 +1,19 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title_UI : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] SceneAsset _hubworld;
+
+    public void ClickStart()
+    {
+        SceneManager.LoadScene(_hubworld.name);
+    }
+
+    public void ClickExit()
+    {
+        Application.Quit();
+    }
+}
