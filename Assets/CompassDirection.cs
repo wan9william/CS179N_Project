7ad9609098d014dfb7ff82322780d4caec928618
Nicholas Karalis <114453356+Googoo03@users.noreply.github.com>
2025-06-06 10:06:03 -0700

    Changed Compass pivot

--- Assets/CompassDirection.cs
+++ Assets/CompassDirection.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using static UnityEngine.GraphicsBuffer;
 
 public class CompassDirection : MonoBehaviour
 {
@@ -31,8 +32,12 @@ public class CompassDirection : MonoBehaviour
     {
         if (player == null || ship == null) return;
 
-        Vector2 direction = (ship.position - player.position).normalized;
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        rectTransform.rotation = Quaternion.Euler(0, 0, angle - 90); // -90 to point up
+        Vector2 direction = (ship.position - player.position);
+        //rectTransform.LookAt(direction);
+
+        //Vector2 direction = target.position - transform.position;
+        transform.rotation = Quaternion.FromToRotation(Vector3.up, direction);
+        //float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        //rectTransform.rotation = Quaternion.Euler(0, 0, angle - 90); // -90 to point up
     }
 }
